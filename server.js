2c928fc45f5a6c4ad1d46c80a11a205e0c79de4f
krizyosra <krizyosra50@gmail.com>
2024-09-27 13:33:16 +0200

    cors chatgpt

--- server.js
+++ server.js
@@ -11,16 +11,29 @@ const RoutesProduct=require('./routes/RoutesProduct')
 const Routeuser= require("./routes/RoutesUSer")
 const cors = require("cors");
 
-const corsOptions = {
-    origin: 'https://client-jade-chi.vercel.app/', 
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
-    allowedHeaders: ['Content-Type', 'Authorization'], 
-    credentials: true, 
-  };
+//const corsOptions = {
+  /////  origin: 'https://client-jade-chi.vercel.app/', 
+   // methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+   // allowedHeaders: ['Content-Type', 'Authorization'], 
+  //  credentials: true, 
+  //};
+  const allowedOrigins = ['https://client-jade-chi.vercel.app'];
 
+  const corsOptions = {
+    origin: function (origin, callback) {
+      if (allowedOrigins.includes(origin) || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true,  // Si vous travaillez avec des cookies ou des sessions
+  };
+  
+  app.use(cors(corsOptions));
 
 connect()
-app.use(cors(corsOptions))
+//app.use(cors(corsOptions))
 
 app.options('*', cors(corsOptions));
 app.use(express.json())
